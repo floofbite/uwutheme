163a830a71e71b6524d4445e500310c2144f83df
Dolly Chang (張佩雯) <dollychang@qnap.com>
2024-08-08 10:45:06 +0800

    hotfix error

--- javascripts/discourse/components/custom-homepage-content.js
+++ javascripts/discourse/components/custom-homepage-content.js
@@ -1,17 +1,26 @@
 import Component from "@glimmer/component";
 import { inject as service } from "@ember/service";
-import { defaultHomepage, getCurrentUser } from "discourse/lib/utilities";
+import { defaultHomepage } from "discourse/lib/utilities";
+import { withPluginApi } from "discourse/lib/plugin-api";
 
 export default class CustomHomepageContent extends Component {
-    @service router;
+  @service router;
 
-    get isHomepage() {
-        const { currentRouteName } = this.router;
+  constructor() {
+    super(...arguments);
 
-        return currentRouteName === `discovery.${defaultHomepage()}`;
-    }
+    // Set up the plugin API to get the current user
+    withPluginApi('0.8.18', (api) => {
+      this.api = api;
+    });
+  }
 
-    get isUserLoggedIn() {
-        return getCurrentUser() !== null;
-    }
+  get isHomepage() {
+    const { currentRouteName } = this.router;
+    return currentRouteName === `discovery.${defaultHomepage()}`;
+  }
+
+  get isUserLoggedIn() {
+    return this.api?.getCurrentUser() !== null;
+  }
 }
