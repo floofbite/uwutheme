3e6d6f1d1913f9bcf62ddeda7ec04615a66b3b7c
Dolly Chang (張佩雯) <dollychang@qnap.com>
2024-08-08 14:15:52 +0800

    updated status api

--- javascripts/discourse/components/status.js
+++ javascripts/discourse/components/status.js
@@ -4,27 +4,16 @@ import { ajax } from 'discourse/lib/ajax';
 export default Component.extend({
     didInsertElement() {
         this._super(...arguments);
-        const dailyUserCountApiUrl = 'https://testing-community.qnap.com.tw/directory_items.json?period=daily&order=topic_count';
-        ajax(dailyUserCountApiUrl, {
+        const siteApiUrl = 'https://testing-community.qnap.com.tw/site/statistics.json';
+        ajax(siteApiUrl, {
             method: 'GET'
         }).then((data) => {
-            const dailyUserCount = data.directory_items.length;
-            this.set('dailyUserCount', dailyUserCount);
-        }).catch((error) => {
-            console.error('Error fetching users:', error);
-        });
-
-        const postsCountApiUrl = 'https://testing-community.qnap.com.tw/categories.json';
-        ajax(postsCountApiUrl, {
-            method: 'GET'
-        }).then((data) => {
-            const categories = data.category_list.categories;
-            const visibleCategories = categories.filter(category => !category.read_restricted);
-            const postsCount = visibleCategories.reduce((acc, category) => acc + category.post_count, 0);
-
+            const userCount = data.users_count;
+            const postsCount = data.posts_count;
+            this.set('userCount', userCount);
             this.set('postsCount', postsCount);
         }).catch((error) => {
-            console.error('Error fetching categories:', error);
+            console.error('Failed to fetch site statistics', error);
         });
     }
 });
