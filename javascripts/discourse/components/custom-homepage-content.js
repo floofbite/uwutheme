8957daa80be663555a655186e0352ba0397cf8bd
Dolly Chang (張佩雯) <dollychang@qnap.com>
2024-08-09 10:34:06 +0800

    homepage sidebar

--- javascripts/discourse/components/custom-homepage-content.js
+++ javascripts/discourse/components/custom-homepage-content.js
@@ -2,34 +2,47 @@ import Component from "@glimmer/component";
 import { inject as service } from "@ember/service";
 import { defaultHomepage } from "discourse/lib/utilities";
 import { withPluginApi } from "discourse/lib/plugin-api";
+import { action } from "@ember/object";
 
 export default class CustomHomepageContent extends Component {
-    @service router;
+  @service router;
 
-    constructor() {
-        super(...arguments);
+  constructor() {
+    super(...arguments);
 
-        // Set up the plugin API to get the current user
-        withPluginApi('0.8.18', (api) => {
-            this.api = api;
-        });
+    // Set up the plugin API to get the current user
+    withPluginApi("0.8.18", (api) => {
+      this.api = api;
+    });
+  }
+
+  get isHomepage() {
+    const { currentRouteName } = this.router;
+    return currentRouteName === `discovery.${defaultHomepage()}`;
+  }
+
+  get isUserLoggedIn() {
+    return this.api?.getCurrentUser() !== null;
+  }
+
+  @action
+  handleHomepageClass() {
+    if (this.isHomepage) {
+      document.body.classList.add("homepage");
+    } else {
+      document.body.classList.remove("homepage");
     }
+  }
 
-    get isHomepage() {
-        const { currentRouteName } = this.router;
-        return currentRouteName === `discovery.${defaultHomepage()}`;
-    }
+  @action
+  setupSidebarVisibility() {
+    const applicationController = this.api.container.lookup("controller:application");
+    applicationController.set("showSidebar", !this.isHomepage);
+  }
 
-    get isUserLoggedIn() {
-        return this.api?.getCurrentUser() !== null;
-    }
-
-    didInsertElement() {
-        this._super(...arguments);
-        if (this.isHomepage) {
-            document.body.classList.add("homepage");
-            const applicationController = this.api.container.lookup("controller:application");
-            applicationController.set("showSidebar", !isHomepage);
-        }
-    }
+  // Runs after the component is inserted into the DOM
+  willRender() {
+    this.handleHomepageClass();
+    this.setupSidebarVisibility();
+  }
 }
