08199f157d78290f1ae336327dd40c4b519090d5
Dolly Chang (張佩雯) <dollychang@qnap.com>
2024-08-09 10:40:51 +0800

    fix

--- javascripts/discourse/components/custom-homepage-content.js
+++ javascripts/discourse/components/custom-homepage-content.js
@@ -2,41 +2,27 @@ import Component from "@glimmer/component";
 import { inject as service } from "@ember/service";
 import { defaultHomepage } from "discourse/lib/utilities";
 import { withPluginApi } from "discourse/lib/plugin-api";
-import { action } from "@ember/object";
+// Remove the unused import statement for 'action'
 
 export default class CustomHomepageContent extends Component {
-  @service router;
+    @service router;
 
-  constructor() {
-    super(...arguments);
-
-    withPluginApi("0.8.18", (api) => {
-      this.api = api;
-      this.setupSidebarVisibility();
-    });
-  }
-
-  get isHomepage() {
-    const { currentRouteName } = this.router;
-    return currentRouteName === `discovery.${defaultHomepage()}`;
-  }
-
-  get isUserLoggedIn() {
-    return this.api?.getCurrentUser() !== null;
-  }
-
-  @action
-  setupSidebarVisibility() {
-    const sidebarElement = document.querySelector('.sidebar'); // Adjust the selector as needed
-
-    if (this.isHomepage && sidebarElement) {
-      sidebarElement.style.display = 'none'; // Hide the sidebar on the homepage
-    } else if (sidebarElement) {
-      sidebarElement.style.display = ''; // Show the sidebar on other pages
+    get isHomepage() {
+        const { currentRouteName } = this.router;
+        return currentRouteName === `discovery.${defaultHomepage()}`;
     }
-  }
 
-  willRender() {
-    this.setupSidebarVisibility();
-  }
+    get isUserLoggedIn() {
+        return this.api?.getCurrentUser() !== null;
+    }
+
+    constructor() {
+        super(...arguments);
+
+        withPluginApi("0.8.18", (api) => {
+            this.api = api;
+            const applicationController = api.container.lookup("controller:application");
+            applicationController.set("showSidebar", !this.isHomepage);
+        });
+    }
 }
