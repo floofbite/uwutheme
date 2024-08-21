5c8569e71308293802a4e6cf2ce7f4107af46cb6
Dolly Chang (張佩雯) <dollychang@qnap.com>
2024-08-21 14:16:39 +0800

    udpated style

--- javascripts/discourse/initializers/homepage-sidebar.js
+++ javascripts/discourse/initializers/homepage-sidebar.js
@@ -10,15 +10,12 @@ export default {
                 const isHomepage = currentRoute === `discovery.${defaultHomepage()}`;
                 const applicationController = api.container.lookup("controller:application");
                 const mainOutlet = document.getElementById("main-outlet");
-                const sideBar = document.getElementsByClassName("sidebar-sections")[0];
                 if (isHomepage) {
                     applicationController.set("showSidebar", false);
                     mainOutlet.classList.add("isHomepage");
-                    sideBar.classList.add("isHomepage");
                 } else {
                     applicationController.set("showSidebar", true);
                     mainOutlet.classList.remove("isHomepage");
-                    sideBar.classList.remove("isHomepage");
                 }
             });
         });
