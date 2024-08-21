617de0463bfc542bc61889fc3d69174f1207fa52
Dolly Chang (張佩雯) <dollychang@qnap.com>
2024-08-21 15:44:32 +0800

    updated order

---
+++ javascripts/discourse/components/after-topic-list.js
@@ -0,0 +1,21 @@
+import Component from "@glimmer/component";
+import { inject as service } from "@ember/service";
+import { defaultHomepage } from "discourse/lib/utilities";
+import { withPluginApi } from "discourse/lib/plugin-api";
+
+export default class AfterTopicListContent extends Component {
+    @service router;
+
+    get isHomepage() {
+        const { currentRouteName } = this.router;
+        return currentRouteName === `discovery.${defaultHomepage()}`;
+    }
+
+    constructor() {
+        super(...arguments);
+
+        withPluginApi("0.8.18", (api) => {
+            this.api = api;
+        });
+    }
+}
