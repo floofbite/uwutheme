ee43f8905baf936981ce96d4535c14ca1599e4f4
Dolly Chang (張佩雯) <dollychang@qnap.com>
2024-08-21 15:29:18 +0800

    test

--- javascripts/discourse/components/cards.js
+++ javascripts/discourse/components/cards.js
@@ -9,21 +9,21 @@ export default Component.extend({
                 "target": "_blank",
                 "title": "Blog",
                 "description": "Stay tuned for blog posts about QNAP's products and technologies.",
-                "icon": themeblogLogo
+                "icon": $blogLogo
             },
             {
                 "url": "https://www.qnap.com/en/how-to/search",
                 "target": "_blank",
                 "title": "Knowledge Base",
                 "description": "Explore guides, tutorials, and expert tips to get the most out of your QNAP products.",
-                "icon": knowledgeLogo
+                "icon": $knowledgeLogo
             },
             {
                 "url": "https://www.qnap.com/en/release-notes/",
                 "target": "_blank",
                 "title": "Release Notes",
                 "description": "Discover the latest features, improvements, and bug fixes in our most recent releases.",
-                "icon": releaseLogo
+                "icon": $releaseLogo
             }
         ];
 
