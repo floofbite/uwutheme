64817360569e0c541eaa17f5817d094e0665af64
Dolly Chang (張佩雯) <dollychang@qnap.com>
2024-08-23 10:06:36 +0800

    testing

--- javascripts/discourse/initializers/customize.js
+++ javascripts/discourse/initializers/customize.js
@@ -15,36 +15,36 @@ export default {
                     applicationController.set("showSidebar", false);
                     main.classList.add("isHomepage");
 
-                    document.addEventListener("DOMContentLoaded", function() {
-                        const featureListWrapper = document.getElementsByClassName("featured-lists__wrapper")[0];
-                        const featureListContainer = document.getElementsByClassName("featured-lists__list-container")[0];
-                        const featureListHeader = document.getElementsByClassName("featured-lists__list-header")[0];
+                    console.log('isHomepage start!');
+                    const featureListWrapper = document.getElementsByClassName("featured-lists__wrapper")[0];
+                    const featureListContainer = document.getElementsByClassName("featured-lists__list-container")[0];
+                    const featureListHeader = document.getElementsByClassName("featured-lists__list-header")[0];
 
-                        if (featureListWrapper) {
-                            featureListWrapper.classList.add("full-width");
+                    if (featureListWrapper) {
+                        featureListWrapper.classList.add("full-width");
+                    }
+
+                    if (featureListContainer) {
+                        featureListContainer.classList.add("contents");
+                    }
+
+                    if (featureListHeader) {
+                        const featureListHeaderButton = featureListHeader.querySelector("button");
+                        const featureListHeaderTitle = featureListHeader.querySelector("h2");
+                        const featureListHeaderLink = featureListHeader.querySelector("a");
+                        if (featureListHeaderTitle) {
+                            featureListHeaderTitle.innerText = "Latest";
                         }
 
-                        if (featureListContainer) {
-                            featureListContainer.classList.add("contents");
+                        if (featureListHeaderLink) {
+                            featureListHeaderLink.innerText = "All Topics";
                         }
 
-                        if (featureListHeader) {
-                            const featureListHeaderButton = featureListHeader.querySelector("button");
-                            const featureListHeaderTitle = featureListHeader.querySelector("h2");
-                            const featureListHeaderLink = featureListHeader.querySelector("a");
-                            if (featureListHeaderTitle) {
-                                featureListHeaderTitle.innerText = "Latest";
-                            }
-
-                            if (featureListHeaderLink) {
-                                featureListHeaderLink.innerText = "All Topics";
-                            }
-
-                            if (featureListHeaderButton) {
-                                featureListHeaderButton.innerHTML = '<svg class="fa d-icon d-icon-plus svg-icon svg-string" xmlns="http://www.w3.org/2000/svg"><use href="#plus"></use></svg> <span class="d-button-label">New Topic</span>';
-                            }
+                        if (featureListHeaderButton) {
+                            featureListHeaderButton.innerHTML = '<svg class="fa d-icon d-icon-plus svg-icon svg-string" xmlns="http://www.w3.org/2000/svg"><use href="#plus"></use></svg> <span class="d-button-label">New Topic</span>';
                         }
-                    });
+                    }
+                    console.log('isHomepage end!');
 
                 } else {
                     applicationController.set("showSidebar", true);
