828ee2ab8c61e8f7d427279040c7247950590bc8
Dolly Chang (張佩雯) <dollychang@qnap.com>
2024-08-26 17:05:07 +0800

    test

--- javascripts/discourse/components/cards.js
+++ javascripts/discourse/components/cards.js
@@ -1,24 +1,25 @@
 import Component from "@ember/component";
-import { themeUpload } from "discourse/lib/theme-settings";
 
 export default Component.extend({
-  async didInsertElement() {
-    const siteLang = document.documentElement.getAttribute("lang").toLowerCase();
+    async didInsertElement() {
+      const siteLang = document.documentElement.getAttribute("lang").toLowerCase();
 
-    const jsonFilePath = themeUpload(`/json/${siteLang}.json`);
+      // 使用 themeUpload helper 获取正确的 URL
+      const jsonFilePath = themeUpload(`/json/${siteLang}.json`);
 
-    try {
-      const response = await fetch(jsonFilePath);
+      try {
+        const response = await fetch(jsonFilePath);
 
-      if (response.ok) {
-        const langJson = await response.json();
-        const cardsData = langJson.cards;
-        this.set("cards", cardsData);
-      } else {
-        console.error("error fetching language JSON file", themeUpload(`/json/${siteLang}.json`));
+        if (response.ok) {
+          const langJson = await response.json();
+          const cardsData = langJson.cards;
+
+          this.set("cards", cardsData);
+        } else {
+          console.error("无法加载语言 JSON 文件");
+        }
+      } catch (error) {
+        console.error("获取语言 JSON 文件时出错:", error);
       }
-    } catch (error) {
-      console.error("获取语言 JSON 文件时出错:", error);
     }
-  }
-});
+  });
