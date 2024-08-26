5ca488b0790a6c5cf1f89a1ddaa9ee83c53376e9
Dolly Chang (張佩雯) <dollychang@qnap.com>
2024-08-26 17:11:26 +0800

    test

--- javascripts/discourse/components/cards.js
+++ javascripts/discourse/components/cards.js
@@ -1,23 +1,27 @@
 import Component from "@ember/component";
-import { themeUpload } from "discourse/lib/theme-settings";
+import { getOwner } from "@ember/application";
 
 export default Component.extend({
-    async didInsertElement() {
-      const siteLang = document.documentElement.getAttribute("lang").toLowerCase();
-      const jsonFilePath = `/theme-javascripts/discourse-theme-custom/assets/json/${siteLang}.json`;
-      try {
-        const response = await fetch(jsonFilePath);
+  async didInsertElement() {
+    const siteLang = document.documentElement.getAttribute("lang").toLowerCase();
 
-        if (response.ok) {
-          const langJson = await response.json();
-          const cardsData = langJson.cards;
+    // 使用 getOwner 获取主题上传的前缀
+    const themePrefix = getOwner(this).resolveRegistration('config:theme-settings')['theme_uploads_prefix'];
+    const jsonFilePath = `${themePrefix}/json/${siteLang}.json`;
 
-          this.set("cards", cardsData);
-        } else {
-          console.error("无法加载语言 JSON 文件");
-        }
-      } catch (error) {
-        console.error("获取语言 JSON 文件时出错:", error);
+    try {
+      const response = await fetch(jsonFilePath);
+
+      if (response.ok) {
+        const langJson = await response.json();
+        const cardsData = langJson.cards;
+
+        this.set("cards", cardsData);
+      } else {
+        console.error("无法加载语言 JSON 文件");
       }
+    } catch (error) {
+      console.error("获取语言 JSON 文件时出错:", error);
     }
-  });
+  }
+});
