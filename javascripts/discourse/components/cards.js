9396b70a703de36a0731a8408daf8286cf109970
Dolly Chang (張佩雯) <dollychang@qnap.com>
2024-08-26 16:03:09 +0800

    test

--- javascripts/discourse/components/cards.js
+++ javascripts/discourse/components/cards.js
@@ -1,30 +1,11 @@
-import Component from "@ember/component";
 
 export default Component.extend({
-    didInsertElement() {
-        const cardsData = [
-            {
-                "url": "https://blog.qnap.com/en/",
-                "target": "_blank",
-                "title": "Blog",
-                "description": "Stay tuned for blog posts about QNAP's products and technologies.",
-                "icon": "blogLogo"
-            },
-            {
-                "url": "https://www.qnap.com/en/how-to/search",
-                "target": "_blank",
-                "title": "Knowledge Base",
-                "description": "Explore guides, tutorials, and expert tips to get the most out of your QNAP products.",
-                "icon": "knowledgeLogo"
-            },
-            {
-                "url": "https://www.qnap.com/en/release-notes/",
-                "target": "_blank",
-                "title": "Release Notes",
-                "description": "Discover the latest features, improvements, and bug fixes in our most recent releases.",
-                "icon": "releaseLogo"
-            }
-        ];
+    async didInsertElement() {
+        const siteLang = document.documentElement.getAttribute("lang").toLowerCase();
+
+        const langJson = await import(`../../../assets/json/${siteLang}.json`);
+
+        const cardsData = langJson.cards;
 
         this.set("cards", cardsData);
     }
