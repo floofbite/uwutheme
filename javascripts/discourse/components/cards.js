2a4954a994796c14671290b033c947c9314900a9
Dolly Chang (張佩雯) <dollychang@qnap.com>
2024-08-26 17:20:54 +0800

    test

--- javascripts/discourse/components/cards.js
+++ javascripts/discourse/components/cards.js
@@ -1,13 +1,11 @@
 import Component from "@ember/component";
-import { getOwner } from "@ember/application";
+import themePrefix from "discourse/helpers/theme-prefix";
 
 export default Component.extend({
   async didInsertElement() {
     const siteLang = document.documentElement.getAttribute("lang").toLowerCase();
 
-    // 使用 getOwner 获取主题上传的前缀
-    const themePrefix = getOwner(this).resolveRegistration('config:theme-settings')['theme_uploads_prefix'];
-    const jsonFilePath = `${themePrefix}/json/${siteLang}.json`;
+    const jsonFilePath = `${themePrefix}/assets/${siteLang}.json`;
 
     try {
       const response = await fetch(jsonFilePath);
@@ -15,10 +13,9 @@ export default Component.extend({
       if (response.ok) {
         const langJson = await response.json();
         const cardsData = langJson.cards;
-
         this.set("cards", cardsData);
       } else {
-        console.error("无法加载语言 JSON 文件");
+        console.error("error fetching language JSON file", `${themePrefix}/assets/json/${siteLang}.json`);
       }
     } catch (error) {
       console.error("获取语言 JSON 文件时出错:", error);
