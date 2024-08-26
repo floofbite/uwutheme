06c5c41ec16abf68238fe093dda868fc8bbe23b0
Dolly Chang (張佩雯) <dollychang@qnap.com>
2024-08-26 17:26:45 +0800

    test

--- javascripts/discourse/components/cards.js
+++ javascripts/discourse/components/cards.js
@@ -2,24 +2,30 @@ import Component from "@ember/component";
 import { getOwner } from "@ember/application";
 
 export default Component.extend({
-  async didInsertElement() {
-    const siteLang = document.documentElement.getAttribute("lang").toLowerCase();
+    async didInsertElement() {
+        const siteLang = document.documentElement.getAttribute("lang").toLowerCase();
 
-    const themePrefix = getOwner(this).resolveRegistration('config:theme-settings')['theme_uploads_prefix'];
-    const jsonFilePath = `${themePrefix}/json/${siteLang}.json`;
+        const themeSettings = getOwner(this).resolveRegistration('config:theme-settings');
 
-    try {
-      const response = await fetch(jsonFilePath);
+        if (themeSettings) {
+            const themePrefix = themeSettings.theme_uploads_prefix;
+            const jsonFilePath = `${themePrefix}/json/${siteLang}.json`;
 
-      if (response.ok) {
-        const langJson = await response.json();
-        const cardsData = langJson.cards;
-        this.set("cards", cardsData);
-      } else {
-        console.error("error fetching language JSON file", `${themePrefix}/assets/json/${siteLang}.json`);
-      }
-    } catch (error) {
-      console.error("获取语言 JSON 文件时出错:", error);
+            try {
+                const response = await fetch(jsonFilePath);
+
+                if (response.ok) {
+                    const langJson = await response.json();
+                    const cardsData = langJson.cards;
+                    this.set("cards", cardsData);
+                } else {
+                    console.error("error fetching language JSON file", `${themePrefix}/assets/json/${siteLang}.json`);
+                }
+            } catch (error) {
+                console.error("获取语言 JSON 文件时出错:", error);
+            }
+        } else {
+            console.error('Theme settings are not correctly registered.');
+        }
     }
-  }
 });
