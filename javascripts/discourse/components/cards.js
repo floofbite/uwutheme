417917223eedb309a4b5e6b72f08e4407d4a7a9f
Dolly Chang (張佩雯) <dollychang@qnap.com>
2024-08-27 09:00:33 +0800

    test

--- javascripts/discourse/components/cards.js
+++ javascripts/discourse/components/cards.js
@@ -4,6 +4,7 @@ import i18n from 'discourse-common/helpers/i18n';
 export default Component.extend({
     didInsertElement() {
         const cardsData = i18n(themePrefix + 'cards');
+        console.log(cardsData);
 
         this.set("cards", cardsData);
     }
