78887cbda44d6ecdf71c0669f0d6a5beaccddf02
Dolly Chang (張佩雯) <dollychang@qnap.com>
2024-08-27 15:29:37 +0800

    added category zh-tw

--- javascripts/discourse/components/category-list.js
+++ javascripts/discourse/components/category-list.js
@@ -2,21 +2,39 @@ import Component from '@ember/component';
 import { ajax } from 'discourse/lib/ajax';
 
 export default Component.extend({
-  didInsertElement() {
-    this._super(...arguments);
+    didInsertElement() {
+        this._super(...arguments);
 
-      const apiUrl = '/categories.json';
+        const apiUrl = '/categories.json';
 
-      ajax(apiUrl, {
-        method: 'GET'
-      }).then((data) => {
-        const categories = data.category_list.categories;
-        const visibleCategories = categories.filter(category => !category.read_restricted);
+        ajax(apiUrl, {
+            method: 'GET'
+        }).then((data) => {
+            const categories = data.category_list.categories;
+            const visibleCategories = categories.filter(category => !category.read_restricted);
+            // visibleCategories get slug put in langsReplaceByI18n
+            // const langsReplaceByI18n = [
+            //     { slug: "" , title: I18n.t(themePrefix("category.general.title")), description: I18n.t(themePrefix("category.general.description")) },
+            // ];
+            const langsReplaceByI18n = [];
+            visibleCategories.forEach(category => {
+                let slug = category.slug;
+                if (slug.indexOf('-') !== -1) {
+                    slug = slug.replace('-', '_');
+                }
+                langsReplaceByI18n.push({
+                    slug: category.slug,
+                    name: I18n.t(themePrefix("category." + category.slug + ".name")),
+                    description:  I18n.t(themePrefix("category." + category.slug + ".description")),
+                    uploaded_logo: category.uploaded_logo,
+                    id: category.id
+                });
+            });
 
-        // Set filtered categories data to the component
-        this.set('categories', visibleCategories);
-      }).catch((error) => {
-        console.error('Error fetching categories:', error);
-      });
-  }
+            this.set('categories', langsReplaceByI18n);
+            // this.set('categories', visibleCategories);
+        }).catch((error) => {
+            console.error('Error fetching categories:', error);
+        });
+    }
 });
