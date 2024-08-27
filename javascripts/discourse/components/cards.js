1fd4ed9fd3598d5c7be0e3d795cbabbd3bd4ed91
Dolly Chang (張佩雯) <dollychang@qnap.com>
2024-08-27 11:19:09 +0800

    test

--- javascripts/discourse/components/cards.js
+++ javascripts/discourse/components/cards.js
@@ -3,9 +3,9 @@ import I18n from "I18n";
 
 export default Component.extend({
     didInsertElement() {
-        const item1 = I18n.lookup("theme_translations.discourse_theme_custom.qnap_content.cards.item1");
-        const item2 = I18n.lookup("theme_translations.discourse_theme_custom.qnap_content.cards.item2");
-        const item3 = I18n.lookup("theme_translations.discourse_theme_custom.qnap_content.cards.item3");
+        const item1 = I18n.t("theme_translations.discourse_theme_custom.qnap_content.cards.item1");
+        const item2 = I18n.t("theme_translations.discourse_theme_custom.qnap_content.cards.item2");
+        const item3 = I18n.t("theme_translations.discourse_theme_custom.qnap_content.cards.item3");
 
         const cardsData = [item1, item2, item3];
 
