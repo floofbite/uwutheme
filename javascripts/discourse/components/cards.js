46f6fd4fd4ce841e3e7da01b75760cd392fad156
Dolly Chang (張佩雯) <dollychang@qnap.com>
2024-08-27 09:34:36 +0800

    reset

--- javascripts/discourse/components/cards.js
+++ javascripts/discourse/components/cards.js
@@ -1,14 +1,31 @@
 import Component from "@ember/component";
-import I18n from "discourse-i18n";
 
 export default Component.extend({
     didInsertElement() {
-        const themePrefix = 'discourse-theme-custom';
-        const translationKey = `theme_translations.${themePrefix}.qnap_content.test`;
-        const cardsData = I18n.t(translationKey);
+        const cardsData = [
+            {
+                "url": "https://blog.qnap.com/en/",
+                "target": "_blank",
+                "title": "Blog",
+                "description": "Stay tuned for blog posts about QNAP's products and technologies.",
+                "icon": "blogLogo"
+            },
+            {
+                "url": "https://www.qnap.com/en/how-to/search",
+                "target": "_blank",
+                "title": "Knowledge Base",
+                "description": "Explore guides, tutorials, and expert tips to get the most out of your QNAP products.",
+                "icon": "knowledgeLogo"
+            },
+            {
+                "url": "https://www.qnap.com/en/release-notes/",
+                "target": "_blank",
+                "title": "Release Notes",
+                "description": "Discover the latest features, improvements, and bug fixes in our most recent releases.",
+                "icon": "releaseLogo"
+            }
+        ];
 
-        console.log(cardsData);
         this.set("cards", cardsData);
     }
 });
-
