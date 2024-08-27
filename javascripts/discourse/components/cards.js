68ef564ff30175d74aa953431d1d4d4cd1013027
Dolly Chang (張佩雯) <dollychang@qnap.com>
2024-08-27 09:01:59 +0800

    test

--- javascripts/discourse/components/cards.js
+++ javascripts/discourse/components/cards.js
@@ -1,6 +1,8 @@
 import Component from "@ember/component";
 import i18n from 'discourse-common/helpers/i18n';
 
+const themePrefix = 'discourse-theme-custom.'; // Define your theme prefix here
+
 export default Component.extend({
     didInsertElement() {
         const cardsData = i18n(themePrefix + 'cards');
