000e5561922427db6ca6d11cbfa7f6b337c5eb9a
Dolly Chang (張佩雯) <dollychang@qnap.com>
2024-08-27 11:38:05 +0800

    test

--- javascripts/discourse/components/cards.js
+++ javascripts/discourse/components/cards.js
@@ -1,14 +1,10 @@
 import Component from "@ember/component";
-import I18n from "discourse-i18n";
+import I18n from "I18n";
 
 export default Component.extend({
-    didInsertElement() {
-        const item1 = I18n.t("theme_translations.discourse_theme_custom.qnap_content.cards.item1");
-        const item2 = I18n.t("theme_translations.discourse_theme_custom.qnap_content.cards.item2");
-        const item3 = I18n.t("theme_translations.discourse_theme_custom.qnap_content.cards.item3");
-
-        const cardsData = [item1, item2, item3];
-
-        console.log(cardsData);
+    get cardsContent() {
+        return I18n.t(themePrefix("cards"), {
+            topicUrl: this.category.topic_url,
+        });
     }
 });
