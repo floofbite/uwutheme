afc8cd4225e5ff386d490dc50edf0ba84cd91001
Dolly Chang (張佩雯) <dollychang@qnap.com>
2024-08-27 08:55:41 +0800

    test i18

--- javascripts/discourse/components/cards.js
+++ javascripts/discourse/components/cards.js
@@ -1,30 +1,9 @@
 import Component from "@ember/component";
+import { I18n } from "discourse/lib/i18n";
 
 export default Component.extend({
     didInsertElement() {
-        const cardsData = [
-            {
-                "url": "https://blog.qnap.com/en/",
-                "target": "_blank",
-                "title": "Blog",
-                "description": "Stay tuned for blog posts about QNAP's products and technologies.",
-                "icon": "blogLogo"
-            },
-            {
-                "url": "https://www.qnap.com/en/how-to/search",
-                "target": "_blank",
-                "title": "Knowledge Base",
-                "description": "Explore guides, tutorials, and expert tips to get the most out of your QNAP products.",
-                "icon": "knowledgeLogo"
-            },
-            {
-                "url": "https://www.qnap.com/en/release-notes/",
-                "target": "_blank",
-                "title": "Release Notes",
-                "description": "Discover the latest features, improvements, and bug fixes in our most recent releases.",
-                "icon": "releaseLogo"
-            }
-        ];
+        const cardsData = I18n.t("cards");
 
         this.set("cards", cardsData);
     }
