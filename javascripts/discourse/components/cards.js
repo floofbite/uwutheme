ccc957c03ae5ca012743dc40a2c60c8de8ba0c28
Dolly Chang (張佩雯) <dollychang@qnap.com>
2024-08-27 11:47:46 +0800

    test

--- javascripts/discourse/components/cards.js
+++ javascripts/discourse/components/cards.js
@@ -2,9 +2,12 @@ import Component from "@ember/component";
 import I18n from "I18n";
 
 export default Component.extend({
-    get cardsContent() {
-        return I18n.t(themePrefix("cards"), {
-            topicUrl: this.category.topic_url,
-        });
-    }
+    // get GetCards() {
+    //     console.log('GetCards'+I18n.t(themePrefix("cards"), {
+    //         defaultValue: []
+    //     }));
+    //     return I18n.t(themePrefix("cards"), {
+    //         defaultValue: []
+    //     });
+    // }
 });
