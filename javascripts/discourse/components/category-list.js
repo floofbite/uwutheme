8b36cc4cb8949abd712c30d69ed363308cada97d
Dolly Chang (張佩雯) <dollychang@qnap.com>
2024-09-03 15:33:09 +0800

    reset category langs

--- javascripts/discourse/components/category-list.js
+++ javascripts/discourse/components/category-list.js
@@ -12,22 +12,12 @@ export default Component.extend({
         }).then((data) => {
             const categories = data.category_list.categories;
             const visibleCategories = categories.filter(category => !category.read_restricted);
-            const langsReplaceByI18n = [];
             visibleCategories.forEach(category => {
-                let slug = category.slug;
-                if (slug.indexOf('-') !== -1) {
-                    slug = slug.replace(/-/g, '_');
-                }
-                langsReplaceByI18n.push({
-                    slug: category.slug,
-                    name: I18n.t(themePrefix("category." + slug + ".name")),
-                    description:  I18n.t(themePrefix("category." + slug + ".description")),
-                    uploaded_logo: category.uploaded_logo,
-                    id: category.id
-                });
+                category.description = category.description;
+                category.name = category.name;
             });
 
-            this.set('categories', langsReplaceByI18n);
+            this.set('categories', visibleCategories);
         }).catch((error) => {
             console.error('Error fetching categories:', error);
         });
