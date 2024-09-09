1d31b1e8ebc277dda8ff94e37a0e327ce625d5fa
Even Lo (羅聖智) <evenlo@qnap.com>
2024-09-09 15:04:33 +0800

    added translation file

--- javascripts/discourse/initializers/translation.js
+++ javascripts/discourse/initializers/translation.js
@@ -5,6 +5,13 @@ export default {
     name: "translation",
     initialize() {
         withPluginApi("0.8.18", (api) => {
+            const locale = I18n.currentLocale();
+            I18n.translations[locale] = I18n.translations[locale] || {};
+            I18n.translations[locale].js = I18n.translations[locale].js || {};
+            I18n.translations[locale].js.login = I18n.translations[locale].js.login || {};
+            I18n.translations[locale].js.login.oauth2_basic = I18n.translations[locale].js.login.oauth2_basic || {};
+            I18n.translations[locale].js.login.oauth2_basic.name = 'QNAP ID';
+
             api.onPageChange(() => {
                 const locale = I18n.currentLocale();
                 I18n.translations[locale] = I18n.translations[locale] || {};
